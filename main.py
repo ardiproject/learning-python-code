d2e2f476880a75e2e7577afe46d2a8b74c268ce6
ardiproject <ardipurnomobbm@gmail.com>
2022-05-24 14:45:52 +0700

    Learning python code

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 """
 Ini adalah demo project pertama dengan Python
+Semua sintaksis bahasa pemrograman terdiri dari :
+1. Sekuensial  : Langkah berurutan
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan  : Mengulangi langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 print("Helo World!")
 print("My Name Is Ardi")
+# sekuensial
+print('ibu berkata,"tolong belikan susu satu botol"')
+print('budi menjawab,"baik bu"')
+print("jangan lupa uangnya minta sama ayah")
